a2d3ab63a3ab8aae8537d4b08bae0685858b9c4d
Vaidehi <144115813+Vaidehi0207@users.noreply.github.com>
2024-02-21 13:45:28 +0000

    datatypes

---
+++ 01_Basics/01_Datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) //we are using node.js not browser
+
+console.log(3+3) 
+console.log("Vaidehi");//codee readilbilty should be high
+
+//dataypes
+let name = "Priya" //string datatype
+let age = 21 //integer 
+let loggedIn = false //boolean datatypes
+
+let state; //undefined
+
+let emailId = null
+
+
+//number->2^53
+//bigint
+//string->""
+//boolean->true/false
+//null->standalone value
+//undefined
+//symbol->unique
+
+//object
+
+console.log(typeof"Priya");
+console.log(typeof null);
+console.log(typeof undefined);
+
