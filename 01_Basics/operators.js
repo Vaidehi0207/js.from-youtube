e95e36ce40e44da648ac06e0e3e56ca8ed7a24b4
Vaidehi <144115813+Vaidehi0207@users.noreply.github.com>
2024-02-22 18:06:38 +0000

    comparisons

---
+++ 01_Basics/operators.js
@@ -0,0 +1,29 @@
+let value = 6;
+let negValue = -value;
+//console.log(negValue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*3);
+//console.log(2**3);
+//console.log(2%2);
+
+let str1 = "my name";
+let str2 = "is priya";
+let str3 = str1 + str2;
+
+//console.log(str3);
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+
+//console.log(+true);
+//console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 102
+gameCounter++;
+console.log(gameCounter);
